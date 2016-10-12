57d66db4e89256bb9ec1b51ad6fd810b0e3ad32e
Huy Nguyen <huyn@iastate.edu>
2016-10-11 22:21:09 -0500

    implement solver
    Provide a solver to suggest letter using frequency

--- CS_572_AI/hang_man.py
+++ CS_572_AI/hang_man.py
@@ -6,6 +6,7 @@
 '''
 import dictionary
 import AI
+from colorama import Fore,Style
 class HangMan(object):
     def __init__(self,chances = 7,word_length = 8, data = None ,multiplayer = 'y'):
         self.chances         = chances
@@ -14,6 +15,9 @@ class HangMan(object):
         self.word            = None # initiate the word as empty, 
                                     # if its normal hangman, then it is a string
                                     # if evil, then it is a bag of possible word
+        
+        self.index_list      = [] # this index_list helps the solver to quicker 
+                                  # reduce the potential words (less comparison)
         
         self.word_length     = word_length
         self.data            = data # pass dictionary object
@@ -30,91 +34,100 @@ class HangMan(object):
         while not self.is_end_game():
             self.play_a_turn() # play a turn
     ''' 
-        function: from dictionary of length word to word, ask user for which word
+        function : from dictionary of length word to word, ask user for which word
                 to choose, then initialize the game with the word and the state
                 of the game (or the string of letter to solve)
-        input  : dic (length_count), dic(length_to_word) from function play
-        output : string (word choice)
+        input    : dic (length_count), dic(length_to_word) from function play
+        output   : string (word choice)
     '''           
     def initialize(self,length_count,length_to_word):
         self.guessed_letters = set()
         # initialize game state 
         for letter in range(self.word_length):
             self.game_state.append('_')
-        
-        
+            
         if self.multiplayer.lower() == 'y':
             # list of the word of such length to choose (print about 50 of them)
-            print ("Here are possible words of such length:")
+            print ("Here are possible words of such length: ")
             if length_count[self.word_length] >=50:
                 print (length_to_word[self.word_length][:50])
             else:
                 print (length_to_word[self.word_length])
-            word = str(input("Please choose a word of length "+str(self.word_length)+":"))
+            word = str(input("Please choose a word of length "+str(self.word_length)+": "))
             while len(word) != self.word_length or word not in length_to_word[self.word_length]:
-                word = str(input("Invalid choice. Please choose a word of length "+str(self.word_length)+":"))
+                word = str(input("Invalid choice. Please choose a word of length "+str(self.word_length)+":" ))
         else:
             ## create an AI to get a word of length based on level.
-            level = str(input("Please choose a level (easy,medium,hard):"))
+            level = str(input("Please choose a level (easy,medium,hard): "))
             # create an instant of AI_computer
-            computer = AI.AI_Computer(level,length_to_word[self.word_length])
+            computer = AI.AI_Computer(level,dictionary.Dictionary(length_to_word[self.word_length]) )
             # choose a word using the level
             word = computer.choose_word()
             print ("word:",word)
         self.word = word # initial our word
+        self.solver.dic = dictionary.Dictionary(length_to_word[self.word_length]) #  object initial our solver data as a dictionary
+        
         
     ''' 
-        function: given the word and chances, ask the user for letter input 
-                  and derive new states, and new chances.
+        function : given the word and chances, ask the user for letter input 
+                  and derive new states, and new chances. Also, it will update wrong guess
+                  for the AI solver
               
-        input  : str(word)
-        output : str(game_state),int(chances)
+        input    : str(word)
+        output   : str(game_state),int(chances)
     '''  
     def play_a_turn(self):
-        print ("You have guessed:",self.guessed_letters)
-        letter = str(input("Please chose an alphabet letter:"))
+        print (Fore.RED +"The letters you have guessed: "+str(self.guessed_letters)+Style.RESET_ALL)
+        # get the letter input from user
+        letter = str(input("Please chose an alphabet letter: "))
         while letter in self.guessed_letters or letter not in self.data.alphabet_letter:
-            letter = str(input("Please provide a proper alphabet letter:"))
+            letter = str(input("Please provide a proper alphabet letter: "))
         # add the letter to guessed_letters
         self.guessed_letters.add(letter)
-        index_list = self.find_all(letter)  # find all the index of letter in word
+        self.find_all(letter)  # find all the index of letter in word
         
-        if len(index_list) ==0: # cant find any
+        if len(self.index_list) ==0: # cant find any
             self.chances -= 1 # update chance if guess wrong
             print ("You have guessed wrongly")
             self.print_state()
         else:
             
-            self.update(index_list,letter)
+            self.update(letter) # else, using the index in our current index list to update the game state
             print ("You have guessed rightly")
             self.print_state()
+            
+        # using the updated index_list, and the letter, our AI class method solve
+        # will reason whether if the letter is a wrong guess or not and provide
+        # next suggestions
+        suggestions = self.solver.solve(self.index_list,letter)
+        # print those suggestions
+        print ("Here are the top 5 most probable letters: ")
+        self.solver.print_top_5(suggestions) 
         
-        
-    ######################################
+    ############################################################################
     # helper functions
     
     # given a right move, update the game state
-    def update(self,index_list,letter):
-        for index in index_list:
+    def update(self,letter):
+        for index in self.index_list:
             self.game_state[index] = letter
             
     # given a letter, find all the index 
     def find_all(self,letter):
-        index_list =[]
+        self.index_list =[]
         for index in range(len(self.word)):
             if self.word[index] == letter:
-                index_list.append(index)
-        return index_list
+                self.index_list.append(index)
             
         
     # print state game and chances left
     def print_state(self):
         print ("You have "+str(self.chances) +" chances left")
-        print ("The string so far is :")
+        print ("The string so far is : ")
         string =""
         for item in self.game_state:
             string+=item+"  "
-        print (string)
+        print (Fore.GREEN + string+ Style.RESET_ALL)
     
     # check if end state of game, it prints the the state of the game
     def is_end_game(self):
@@ -127,7 +140,7 @@ class HangMan(object):
             if self.chances ==0:
                 print ("Oops, you have lost")
                 self.print_state() # print state
-                print ("Here is the word:" + self.word)
+                print ("Here is the word: " + self.word)
                 return True
             else:
                 return False
@@ -136,19 +149,20 @@ class HangMan(object):
  
                     
 def main():
-    dictionary_word = str(input("which dictionary you want to use ('words.txt','words1.txt','words2.txt'):"))
-    chances = int(input("Please indicate how many chances player can have:"))
-    word_length = int(input("Please indicate what length to choose:"))
-    mutiplayer = str(input("Do you want to play with a human(y,n):"))
+    dictionary_word = str(input("which dictionary you want to use ('words.txt','words1.txt','words2.txt'): "))
+    chances = int(input("Please indicate how many chances player can have: "))
+    word_length = int(input("Please indicate what length to choose: "))
+    mutiplayer = str(input("Do you want to play with a human(y,n): "))
     # create a Hangman object
-    game = HangMan(chances,word_length, dictionary.Dictionary(dictionary_word),mutiplayer)
+    game = HangMan(chances,word_length, dictionary.Dictionary(dictionary.parse_text(dictionary_word))
+    ,mutiplayer)
     game.play()
-    string = str(input("Do you want to play again? (y:yes, n : no)"))
+    string = str(input("Do you want to play again? (y:yes, n : no) "))
     while string.lower() == 'y':
-        chances = int(input("Please indicate how many chances player can have:"))
-        word_length = int(input("Please indicate what length to choose:"))
+        chances = int(input("Please indicate how many chances player can have: "))
+        word_length = int(input("Please indicate what length to choose: "))
         game = HangMan(chances,word_length,'words.txt',mutiplayer)
         game.play()    
-        string = str(input("Do you want to play again? (y:yes, n : no)"))
+        string = str(input("Do you want to play again? (y:yes, n : no) "))
     
 main()
