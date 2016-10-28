ffb317fe4eed531f7583c22b15fbabfba42edb2e
Huy Nguyen <huyn@iastate.edu>
2016-10-28 16:07:30 -0500

    refactor play_a_turn
    refactoring several part. 
    For play_a_game, break it into 3 main part, wrote 2 extra helpers function to make it more readable.
    include a play a game where the we don't specify a word (play_a_turn_word / play_a_trun_words) and include the switch to go bck to word

--- CS_572_AI/hang_man.py
+++ CS_572_AI/hang_man.py
@@ -5,6 +5,7 @@
     End     : /2016
 '''
 import dictionary
+import random
 import AI
 from colorama import Fore,Style
 class HangMan(object):
@@ -24,6 +25,12 @@ class HangMan(object):
         self.success         = False # initiate the game as False
         self.multiplayer     = multiplayer # indicate that we play human vs human
         self.solver          = AI.AI_solver() # initiate a solver
+        
+        self.computer        = None # initiate the AI computer to choose ward
+        
+        self.level           = None # initiate the level of the game as nothing
+        self.set_levels      = ["easy","medium","hard","insane"]
+        self.switch          = False # switch level back to easy, medium when the bag only contain 1 word
     # play the game function (main function)
     def play(self):
         # pull the data
@@ -31,9 +38,17 @@ class HangMan(object):
 
         # Initialize the game
         self.initialize(length_count,length_to_word)
-        #  initiate a solver
-        while not self.is_end_game():
-            self.play_a_turn() # play a turn
+        # check the level, and play a turn accordingly
+        if self.level in ["easy","medium"]:
+            while not self.is_end_game():
+                self.play_a_turn_word() # play a turn
+        else:
+            while not self.is_end_game():
+                self.play_a_turn_words() # play a turn
+                if self.switch:
+                    while not self.is_end_game():
+                        self.play_a_turn_word() # play a turn
+                    break # only contain 1 word, switch back, might need lots of test 
     ''' 
         function : from dictionary of length word to word, ask user for which word
                 to choose, then initialize the game with the word and the state
@@ -59,45 +74,53 @@ class HangMan(object):
                 word = str(input("Invalid choice. Please choose a word of length "+str(self.word_length)+":" ))
         else:
             ## create an AI to get a word of length based on level.
-            level = str(input("Please choose a level (easy,medium,hard): "))
+            self.level = str(input("Please choose a level (easy,medium,hard,insane): "))
+            while self.level not in self.set_levels:
+                self.level = str(input("Please choose a proper level (easy,medium,hard,insane): "))
             # create an instant of AI_computer based on level
-            computer = AI.AI_Computer(level,dictionary.Dictionary(length_to_word[self.word_length]) )
+            self.computer = AI.AI_Computer(self.level,dictionary.Dictionary(length_to_word[self.word_length]) )
             # choose a word using the level
-            word = computer.choose_word().lower()
+            word = self.computer.choose_word()
             # print ("word:",word)
-        self.word = word # initial our word
+        self.word = word # initial our word, it could be a bag of word from hard/ insane level
         self.solver.dic = dictionary.Dictionary(length_to_word[self.word_length]) #  object initial our solver data as a dictionary
         
         
     ''' 
-        function : given the word and chances, ask the user for letter input 
-                  and derive new states, and new chances. Also, it will update wrong guess
-                  for the AI solver
+        function : Play a turn if the self.word consist of 1 word. Gven the word and chances, 
+                  ask the user for letter input and derive new states, and new chances. 
+                  Also, it will update wrong guess for the AI solver
               
-        input    : str(word)
-        output   : str(game_state),int(chances)
+        input    : None (use current game state, current guessed letters)
+        output   : None (update current game state or current chances, current guessed letters)
     '''  
-    def play_a_turn(self):
-        print (Fore.RED +"The letters you have guessed: "+str(self.guessed_letters)+Style.RESET_ALL)
-        print ("alphabet_letter: ",self.data.alphabet_letter)
-        # get the letter input from user
-        letter = str(input("Please chose an alphabet letter: "))
-        while letter not in self.data.alphabet_letter or letter in self.guessed_letters:
-            letter = str(input("Please provide a proper alphabet letter: "))
-        # add the letter to guessed_letters
-        self.guessed_letters.add(letter)
-        self.find_all(letter)  # find all the index of letter in word
+    def play_a_turn_word(self):
         
-        if len(self.index_list) ==0: # cant find any
-            self.chances -= 1 # update chance if guess wrong
-            print ("You have guessed wrongly")
-            self.print_state()
-        else:
-            
-            self.update(letter) # else, using the index in our current index list to update the game state
-            print ("You have guessed rightly")
-            self.print_state()
-            
+        letter = self.take_guess() # get the letter from the user
+        self.find_all(letter)  # find all the index of letter in word      
+        self.check_guess(letter) # check if the guess is right or wrong and update the game state
+        
+        # using the updated index_list, and the letter, our AI class method solve
+        # will reason whether if the letter is a wrong guess or not and provide
+        # next suggestions
+        suggestions = self.solver.solve(self.index_list,letter)
+        # print those suggestions
+        print ("Here are the top 5 most probable letters: ")
+        self.solver.print_top_5(suggestions) 
+        
+    ''' 
+        function : Play a turn if the self.word is a list of word. Gven the word and chances, 
+                  ask the user for letter input and derive new states, and new chances. 
+                  Also, it will update wrong guess for the AI solver
+              
+        input    : None (use current game state, current guessed letters)
+        output   : None (update current game state or current chances, current guessed letters)
+    '''  
+    def play_a_turn_words(self):
+        
+        letter = self.take_guess() # get the letter from the user
+        self.word, self.index_list,self.switch =  self.computer.naive_filter(letter)
+        self.check_guess(letter)
         # using the updated index_list, and the letter, our AI class method solve
         # will reason whether if the letter is a wrong guess or not and provide
         # next suggestions
@@ -131,6 +154,30 @@ class HangMan(object):
             string+=item+"  "
         print (Fore.GREEN + string+ Style.RESET_ALL)
     
+    # take the input guess letter from user, return the guess letter
+    def take_guess(self):
+        print (Fore.RED +"The letters you have guessed: "+str(self.guessed_letters)+Style.RESET_ALL)
+        print ("alphabet_letter: ",self.data.alphabet_letter)
+        # get the letter input from user
+        letter = str(input("Please chose an alphabet letter: "))
+        while letter not in self.data.alphabet_letter or letter in self.guessed_letters:
+            letter = str(input("Please provide a proper alphabet letter: "))
+        # add the letter to guessed_letters
+        self.guessed_letters.add(letter)
+        return letter
+    
+    # given the guessed letter as input, using the index_list info, 
+    # determine whether user guessed wrongly or rightly and print the current game state
+    def check_guess(self,letter):
+        if len(self.index_list) ==0: # cant find any
+            self.chances -= 1 # update chance if guess wrong
+            print ("You have guessed wrongly")
+            self.print_state()
+        else: 
+            self.update(letter) # else, using the index in our current index list to update the game state
+            print ("You have guessed rightly")
+            self.print_state()
+            
     # check if end state of game, it prints the the state of the game
     def is_end_game(self):
         if '_' not in self.game_state: # check if the word is solve
@@ -142,27 +189,28 @@ class HangMan(object):
             if self.chances ==0:
                 print ("Oops, you have lost")
                 self.print_state() # print state
-                print ("Here is the word: " + self.word)
+                print ("Here is the word: " + random.choice(self.word))
                 return True
             else:
                 return False
             
                 
- 
-                    
+###############################################################################  
+## main program
+###############################################################################                
 def main():
     dictionary_word = str(input("which dictionary you want to use ('words.txt','words2.txt','words3.txt'): "))
     chances = int(input("Please indicate how many chances player can have: "))
-    word_length = int(input("Please indicate what length to choose: "))
+    word_length = int(input("Please indicate what length to choose (less than 24): "))
     mutiplayer = str(input("Do you want to play with a human(y,n): "))
     # create a Hangman object
     diction = dictionary.Dictionary(dictionary.parse_text(dictionary_word))
     game = HangMan(chances,word_length, diction,mutiplayer)
     game.play()
     string = str(input("Do you want to play again? (y:yes, n : no) "))
-    while string.lower() == 'y':
+    while string.lower() in 'yes':
         chances = int(input("Please indicate how many chances player can have: "))
-        word_length = int(input("Please indicate what length to choose: "))
+        word_length = int(input("Please indicate what length to choose (less than 24): "))
         game = HangMan(chances,word_length,diction,mutiplayer)
         game.play()    
         string = str(input("Do you want to play again? (y:yes, n : no) "))
