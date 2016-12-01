b85eb9896fd672fbcd480903692e4516981f4721
Huy Nguyen <huyn@iastate.edu>
2016-12-01 15:23:52 -0600

    Update fetch_pdb.py

--- CS_569_ProteinStructure/project/fetch_pdb.py
+++ CS_569_ProteinStructure/project/fetch_pdb.py
@@ -11,13 +11,30 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
+###############################################################################
+## helper function
+
+'''
+    function: given operon_classification, write the operon ,and its classification into a text file
+    input   : operon_classification
+    output  : file
+'''
+def to_file(operon_classification):
+    outfile = open('result.txt','w')
+    for operon in operon_classification:
+        outfile.write(operon)
+        for classification in operon_classification[operon]:
+            outfile.write('\t'+classification)
+        outfile.write('\n')
+    outfile.close()
+
+###############################################################################
 # global variables to fethch file
 blast_url         = "http://www.rcsb.org/pdb/rest/getBlastPDB1"
 blast_parameter1  = "?sequence="
 blast_parameter2  = "&eCutOff=1E-20&matrix=BLOSUM62&outputFormat=HTML"
 
 pdb_report_url    = "http://www.rcsb.org/pdb/rest/customReport.csv"
-report_parameter1 = "?pdbids="
 report_parameter2 = "&customReportColumns=classification&service=wsfile&format=csv"
 
 '''
@@ -69,4 +86,45 @@ def retrieve_pdb_ids(dic):
         pdbids_dic[operon] = pdb_list
     return pdbids_dic,pdbids_list
     
-pdbids_dic,pdbids_list = retrieve_pdb_ids(dic)
+# pdbids_dic,pdbids_list = retrieve_pdb_ids(dic)
+
+'''
+    function: using the pdbids_list to retrieve the classification into a dic
+    input   : set(pdbids)
+    output  : dic (key: pdbid,value: classification)
+'''
+
+def retrieve_pdb_classification(pdbids_list):
+    report_parameter1 = "?pdbids="
+    classification_dic ={}
+    for pdb_id in pdbids_list:
+        report_parameter1 += pdb_id+','
+    URL  = pdb_report_url+report_parameter1[:-1]+report_parameter2
+    soup = BeautifulSoup(urlopen(URL)) # fetch the file 
+    text = soup.text
+    text = text.replace('"','')
+    core_info = text.split('\n')[1:-1]
+    for info in core_info:
+        info = info.split(',')
+        classification_dic[info[0]]=info[1]
+    return classification_dic
+    
+# classification_dic = retrieve_pdb_classification(pdbids_list)
+
+'''
+    function: using the info from pdbids_dic, and classification_dic to predict
+              classification for the operon
+    input   : dic (key: pdbid,value: classification), dic (key: operon name,value: list(pdbids))
+    output  : dic (key: operon, value: set of classification)
+'''
+def predict_classification(pdbids_dic,classification_dic):
+    operon_classification = {}
+    for operon in pdbids_dic:
+        classification = set()
+        for pdb_id in pdbids_dic[operon]:
+            classification.add(classification_dic[pdb_id])
+        operon_classification[operon] = classification
+    return operon_classification
+    
+#operon_classification =  predict_classification(pdbids_dic,classification_dic)
+to_file(operon_classification)
