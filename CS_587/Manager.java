93ede2110316ef72ec4773ae5c4c9c96c021e202
Huy Nguyen <huyn@iastate.edu>
2017-02-04 13:31:09 -0600

    create new thread
    Implement BeaconListener thread, make sure the client_IDs are synchronized

--- CS_587/Manager.java
+++ CS_587/Manager.java
@@ -2,7 +2,6 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
-
 class Manager
 {
    public static void main(String args[]) throws Exception
@@ -13,52 +12,81 @@ class Manager
         
 
         System.out.println("Starting server with port: 8888");
-        while (true)
-        {	byte[] receive = new byte[1024]; // reinitiate every time so it wont store extra thing
-        	DatagramPacket receivePacket = new DatagramPacket(receive, receive.length); // initialize empty packet
-        	socket.receive(receivePacket); // receive packet from the port
-        	String mess = new String(receivePacket.getData());
-        	System.out.println("Server has receive this mess:" + mess);
-        	// look into the info of the packet to send back this UDP
-        	
-        	// parse this mess into BEACON structure
-        	String[] parts = mess.split(",");
-        	int cmdPort = Integer.parseInt(parts[6].trim()); // have to trim
-        	
-        	int[] IP_address = new int[4];
-        	for (int i=0;i<4;i++)
-        	{
-        		IP_address[0] = Integer.parseInt(parts[i+2]);
-        	}
-        	// create a BEACON object
-        	BEACON bacon = new BEACON(Integer.parseInt(parts[0]), Integer.parseInt(parts[1]),IP_address, cmdPort);
-        	System.out.println("Successful receiving the BEACON from client ID:"+parts[0]);
-        	// check if the ID is already in the HashTable
-        	if (client_IDs.containsKey(bacon.ID))
-        	{
-        		Status New_Client_Status = new Status(0, false, false, bacon.StartUpTime);
-        		client_IDs.put(bacon.ID,client_IDs.get(bacon.ID).update_status(New_Client_Status));
-        	}
-        	else
-        	{
-        		// put my beautiful bacon into the hashtable
-        		System.out.println("New client is spawn with ID:"+String.valueOf(bacon.ID)+" at time:"+String.valueOf(bacon.StartUpTime));
-        		client_IDs.put(bacon.ID, new Status(0, false, false, bacon.StartUpTime));
-        		new Thread(new ClientAgent(cmdPort)).start();
-        	}
-        	
-//        	if (client_IDs.get(bacon.ID).dead)
-//        	{
-//        		break;
-//        	}
-        		
-
-        		
-        }
-        // socket.close();
+        Thread my_beacon_listener = new Thread(new BeaconListener(socket, client_IDs));
+        my_beacon_listener.start();
+        my_beacon_listener.join();
+        
    }
 }
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+// thread classes
+// BeaconListener Thread
+class BeaconListener implements Runnable{
+	DatagramSocket UDP_socket = null;
+	Map<Integer, Status> client_IDs;
+	public BeaconListener(DatagramSocket socket,Map<Integer, Status> array_client_IDs)
+	{
+		UDP_socket = socket;
+		client_IDs = array_client_IDs;
+	}
+	public void run() 
+	{
+		synchronized(client_IDs) // make sure our resources client_IDs is synchronized
+		{
+			while (true)
+	        {	byte[] receive = new byte[1024]; // reinitiate every time so it wont store extra thing
+	        	DatagramPacket receivePacket = new DatagramPacket(receive, receive.length); // initialize empty packet
+	        	try {
+					UDP_socket.receive(receivePacket);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} // receive packet from the port
+	        	String mess = new String(receivePacket.getData());
+	        	System.out.println("Server has receive this mess:" + mess);
+	        	// look into the info of the packet to send back this UDP
+	        	
+	        	// parse this mess into BEACON structure
+	        	String[] parts = mess.split(",");
+	        	int cmdPort = Integer.parseInt(parts[6].trim()); // have to trim
+	        	
+	        	int[] IP_address = new int[4];
+	        	for (int i=0;i<4;i++)
+	        	{
+	        		IP_address[0] = Integer.parseInt(parts[i+2]);
+	        	}
+	        	// create a BEACON object
+	        	BEACON bacon = new BEACON(Integer.parseInt(parts[0]), Integer.parseInt(parts[1]),IP_address, cmdPort);
+	        	System.out.println("Successful receiving the BEACON from client ID:"+parts[0]);
+	        	// check if the ID is already in the HashTable
+	        	if (client_IDs.containsKey(bacon.ID))
+	        	{
+	        		//if is already in there,need to check whether it was dead and got revived
+	        		// TO DO
+	        	}
+	        	else
+	        	{
+	        		// put my beautiful bacon into the hashtable
+	        		System.out.println("New client is spawn with ID:"+String.valueOf(bacon.ID)+" at time:"+String.valueOf(bacon.StartUpTime));
+	        		client_IDs.put(bacon.ID, new Status(0, false, false, bacon.StartUpTime, System.currentTimeMillis()));
+	        		Thread my_client_agent = new Thread(new ClientAgent(cmdPort));
+	        		my_client_agent.start();
+	        		try 
+	        		{
+						my_client_agent.join();
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+	        	}
+	              		
+	        }
+		}
+	}
+	
+}
 
+// ClientAgent Thread
 class ClientAgent implements Runnable {
 	Socket TCP_socket = null;
 	int tcp_port;
@@ -142,13 +170,15 @@ class Status
 	boolean dead = false; // boolean for status is dead
 	boolean restarted = false; // boolean for status restarted
 	int Time;
+	long Time_stamp;
 
-	public Status(int missed, boolean gone, boolean revived, int time)
+	public Status(int missed, boolean gone, boolean revived, int time,long time_stamp)
 		{
 			Missed_Beacon = missed;
 			dead = gone;
 			restarted = revived;
 			Time = time;
+			Time_stamp = time_stamp;
 		}
 	
 	public Status update_status(Status new_status)
@@ -164,7 +194,7 @@ class Status
 			restarted = true; // update restarted
 		}
 		Time = new_status.Time; // update new Time
-		return new Status(Missed_Beacon,dead,restarted,Time);
+		return new Status(Missed_Beacon,dead,restarted,Time, Time_stamp);
 	}
 	
 	public boolean is_dead()
